63d61cbcd958446708d485bc1f7332caa6bfaa6e
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-27 21:27:39 +0200

    trying

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,22 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
-public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+
+public class Divisors 
+{
+	public static void main (String[] args) 
+	{
+		
+	int chosenNumber = Integer.parseInt(args[0]);
+	int count = 1;
+	int modulus = 0;
+	
+	while (count <= chosenNumber); 
+	{
+		modulus = chosenNumber%count;
+		if (modulus==0)
+		{	
+			System.out.println(modulus);
+		}
+		count = count + 1;	
+	}
+		
 	}
 }
