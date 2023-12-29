3d44d1142a5832e293513d7e4d8216924455d696
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-29 17:55:34 +0200

    yayy

--- Reverse.java
+++ Reverse.java
@@ -1,22 +1,27 @@
 
-public class Reverse {
-	public static void main (String[] args) {
+public class Reverse 
+{
+	public static void main (String[] args) 
+	{
 
-		String chosenWord = (args[0]);
-		String reveredWord = " ";
+		String chosenWord = (args[0]); //romi
+		String reveredWord = "";
 		
-		for (int i = chosenWord.length()-1; i>=0; i--)
-		
-		reveredWord = reveredWord + chosenWord.charAt(i);
+		for (int i = chosenWord.length()-1; i>=0; i--) //i=1
+		{
+			reveredWord = reveredWord + chosenWord.charAt(i); //imor
+		}		
 
 		System.out.println(reveredWord);
-		System.out.println("The middle character is " + (chosenWord.charAt((chosenWord.length()/2)-1);
-		//int wordIsEven = chosenWord.length()%2;
-		 
-		// if (wordIsEven == 0) {
-		// 	 System.out.println("The middle character is " + (chosenWord.charAt((chosenWord.length())/2)-1));
-		// } else {
-		// 	 System.out.println("The middle character is " + (chosenWord.charAt((chosenWord.length())/2);
+
+		
+		if (((chosenWord.length())%2) == 0 ) //4%2==0
+		{
+			System.out.println("The middle character is " + (chosenWord.charAt((chosenWord.length()/2)-1))); 
+		} 
+		else 
+		{
+			System.out.println("The middle character is " + (chosenWord.charAt((chosenWord.length())/2)));
 		 }
 	}
-
+}
