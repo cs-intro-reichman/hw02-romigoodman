ae423ad25e11258985d913d979c27c9a8a03664d
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-29 23:42:06 +0200

    yayyyy

--- Perfect.java
+++ Perfect.java
@@ -1,10 +1,35 @@
 
-public class Perfect {
-	public static void main (String[] args) {
-		int isPerfect = Integer.parseInt(args[0]);
-		int num = isPerfect.Length()-1;
-		for (int i = 0; isPerfect%i==0; i++)
+public class Perfect 
+{
+	public static void main (String[] args) 
+	{
+		
+		int isPerfect = Integer.parseInt(args[0]); //6
+		int sumOfDevisions = 0; 
+		String toBePrinted = "";
+		String finelPrint = "";
+		
+		for (int i = 1; i<isPerfect; i++) //1<6
+		{
+		if (isPerfect%i==0)
+		{
+			sumOfDevisions = sumOfDevisions + i; //1
+			toBePrinted = toBePrinted + (i) + " + "; //1 
+		}
+		}
+		int length = toBePrinted.length();
+		if (toBePrinted.charAt(length-2) == 43){
+			finelPrint = toBePrinted.substring(0,length-2);
+		}
 
-			if sum
-	}
+			if (isPerfect==sumOfDevisions) 
+			{
+			System.out.println( isPerfect + " is a perfect number since " + isPerfect + " = " + finelPrint );
+			}
+				else 
+				{
+				System.out.println( isPerfect + " is not a perfect number");
+				}
+	} 	
 }
+
