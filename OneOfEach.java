41042fca74baed14d256b076689812930831aeab
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-30 15:39:34 +0200

    hurray

--- OneOfEach.java
+++ OneOfEach.java
@@ -1,10 +1,56 @@
 
-/**
- *  Simulates the formation of a family in which the parents decide 
- *  to have children until they have at least one child of each gender.
- */
-public class OneOfEach {
-	public static void main (String[] args) {
-		//// Put your code here
+
+public class OneOfEach 
+{
+	public static void main (String[] args) 
+	{
+		
+		String toBePrinted = "";
+		double randomBoyOrGirl = Math.random();
+		boolean isGirl = (randomBoyOrGirl>=0.5);
+		int numberOfChildren = 0;
+
+		if (isGirl)
+		{
+			toBePrinted = (toBePrinted + "g ");
+			numberOfChildren = numberOfChildren +1;
+
+			while (isGirl)
+			{
+			randomBoyOrGirl = Math.random();
+			isGirl=(randomBoyOrGirl>=0.5);
+			numberOfChildren = numberOfChildren +1;	
+			if (isGirl)
+			{
+			toBePrinted = (toBePrinted + "g ");
+			}
+			else
+			{
+			toBePrinted = (toBePrinted + "b ");
+			}
+			}
+		}
+		
+		else
+		{	
+			toBePrinted = (toBePrinted + "b ");
+			numberOfChildren = numberOfChildren+1;
+			while (!isGirl)
+			{
+			randomBoyOrGirl = Math.random();
+			isGirl=(randomBoyOrGirl>=0.5);
+			numberOfChildren = numberOfChildren + 1;	
+			if (!isGirl)
+			{
+				toBePrinted = (toBePrinted + "b ");
+			}
+			else
+			{
+				toBePrinted = (toBePrinted + "g ");
+			}
+			}
+		}
+		System.out.println(toBePrinted);
+		System.out.println("You made it... and you now have "+ numberOfChildren +" children.");
 	}
 }
