3eaa164d776bad3c45565c87f0594d9ed0207a46
romigoodman <romi.goodman@post.runi.ac.il>
2023-12-30 13:53:00 +0200

    wow

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,22 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		
+		int boardSize = Integer.parseInt(args[0]); //10
+		for (int i=1; i<=boardSize; i++) //1<10
+		{
+			for(int j=1; j<=boardSize; j++) //j<10
+			{
+				if (i%2==0)
+				{
+					System.out.print(" *");	
+				}
+				else 
+				{
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
