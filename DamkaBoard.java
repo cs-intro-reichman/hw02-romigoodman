7ad9d9ddc4c0cee3ffdf6ce197a23c08722a08dc
romigoodman <romi.goodman@post.runi.ac.il>
2024-01-03 17:30:13 +0200

    subbmitting

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,13 +1,13 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
-public class DamkaBoard {
-	public static void main(String[] args) {
+
+public class DamkaBoard 
+{
+	public static void main(String[] args) 
+	{
 		
-		int boardSize = Integer.parseInt(args[0]); //10
-		for (int i=1; i<=boardSize; i++) //1<10
+		int boardSize = Integer.parseInt(args[0]); 
+		for (int i=1; i<=boardSize; i++) 
 		{
-			for(int j=1; j<=boardSize; j++) //j<10
+			for(int j=1; j<=boardSize; j++) 
 			{
 				if (i%2==0)
 				{
--- Perfect.java
+++ Perfect.java
@@ -4,17 +4,17 @@ public class Perfect
 	public static void main (String[] args) 
 	{
 		
-		int isPerfect = Integer.parseInt(args[0]); //6
+		int isPerfect = Integer.parseInt(args[0]); 
 		int sumOfDevisions = 0; 
 		String toBePrinted = "";
 		String finelPrint = "";
 		
-		for (int i = 1; i<isPerfect; i++) //1<6
+		for (int i = 1; i<isPerfect; i++) 
 		{
 		if (isPerfect%i==0)
 		{
-			sumOfDevisions = sumOfDevisions + i; //1
-			toBePrinted = toBePrinted + (i) + " + "; //1 
+			sumOfDevisions = sumOfDevisions + i; 
+			toBePrinted = toBePrinted + (i) + " + ";
 		}
 		}
 		int length = toBePrinted.length();
@@ -26,10 +26,10 @@ public class Perfect
 			{
 			System.out.println( isPerfect + " is a perfect number since " + isPerfect + " = " + finelPrint );
 			}
-				else 
-				{
-				System.out.println( isPerfect + " is not a perfect number");
-				}
+			else 
+			{
+			System.out.println( isPerfect + " is not a perfect number");
+			}
 	} 	
 }
 
