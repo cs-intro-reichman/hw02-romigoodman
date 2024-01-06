8e31083cd4698b86694bb100771d04e9e1bb8331
omrihruni <154349572+omrihruni@users.noreply.github.com>
2024-01-06 10:28:57 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -3,7 +3,7 @@ public class Divisors
 {
 	public static void main (String[] args) 
 	{
-		
+		// #feedback - note the indentation here.
 	int chosenNumber = Integer.parseInt(args[0]);
 	int count = 1;
 	
